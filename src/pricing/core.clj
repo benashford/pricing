f96fc9a0255384f4b9b64b2efa0f4abcdd4d2717
Ben Ashford <ben@bcash.org>
2012-10-21 07:59:54 +0100

    Added support for data tables/lookups, etc.

--- src/pricing/core.clj
+++ src/pricing/core.clj
@@ -5,3 +5,16 @@
   (attr :total 100)
   (attr :tax-rate 0.2)
   (attr :total-inc-tax (* :total (+ :tax-rate 1))))
+
+(defmodel sample2
+  (attr :number-of-employees (+ (in :full-time-employees) (in :part-time-employees)))
+  (attr :price-per-employee (lookup :prices :number-of-employees))
+  (attr :total (* :number-of-employees :price-per-employee))
+
+  (table :prices
+    [1 10.0]
+    [2 9.5]
+    [3 9]
+    [4 9]
+    [5 9]
+    [6 8]))
--- test/pricing/core_test.clj
+++ test/pricing/core_test.clj
@@ -6,3 +6,8 @@
   {:total 100
    :tax-rate 0.2
    :total-inc-tax 120.0})
+
+(fact (sample2 {:full-time-employees 2 :part-time-employees 1}) =>
+  {:number-of-employees 3
+   :price-per-employee 9
+   :total 27})
