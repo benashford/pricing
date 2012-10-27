8753fa1e98d8faa9959a2e8fee0aa2600cb4ed1a
Ben Ashford <ben@bcash.org>
2012-10-27 20:18:37 +0100

    Removed un-necessary examples

--- src/pricing/core.clj
+++ src/pricing/core.clj
@@ -2,24 +2,6 @@
   (:use pricing.engine))
 
 (defmodel sample
-  (attr :total 100)
-  (attr :tax-rate 0.2)
-  (attr :total-inc-tax (* :total (+ :tax-rate 1))))
-
-(defmodel sample2
-  (attr :number-of-employees (+ (in :full-time-employees) (in :part-time-employees)))
-  (attr :price-per-employee (lookup :prices :number-of-employees))
-  (attr :total (* :number-of-employees :price-per-employee))
-
-  (table :prices
-         [1 10.0]
-         [2 9.5]
-         [3 9]
-         [4 9]
-         [5 9]
-         [6 8]))
-
-(defmodel sample3
   (attr :number-of-employees (+ (in :full-time-employees) (in :part-time-employees)))
 
   (item :support
--- test/pricing/core_test.clj
+++ test/pricing/core_test.clj
@@ -4,17 +4,7 @@
 
 ; These test the sample models in the pricing.core namespace
 
-(fact (sample {}) =>
-      {:total 100
-       :tax-rate 0.2M
-       :total-inc-tax 120.0M})
-
-(fact (sample2 {:full-time-employees 2 :part-time-employees 1}) =>
-      {:number-of-employees 3
-       :price-per-employee 9
-       :total 27})
-
-(fact (sample3 {:full-time-employees 4 :part-time-employees 3}) =>
+(fact (sample {:full-time-employees 4 :part-time-employees 3}) =>
       {:number-of-employees 7
        :support {:standard-discount 0.75M
                  :unit-cost 7.5M
