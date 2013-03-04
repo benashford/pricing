6d2d6008ab2f85df18ac8ca1c38278967851f106
Ben Ashford <ben@bcash.org>
2013-03-04 19:25:05 +0000

    Remove lein-swank, now deprecated

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]
-                             [lein-swank "1.4.4"]]}
+  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
              :test {:dependencies [[midje "1.4.0"]]}}
   :main pricing.core)
