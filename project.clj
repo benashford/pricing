7f9079dbce4034a2bc9321a02a6702cc9467dd84
Ben Ashford <ben@bcash.org>
2013-03-04 19:27:58 +0000

    Update Clojure to 1.5.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
              :test {:dependencies [[midje "1.4.0"]]}}
   :main pricing.core)
